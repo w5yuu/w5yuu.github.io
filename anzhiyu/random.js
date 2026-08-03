aa039479764cce80839d0c383a9466944587857f
w5yuu <g73ubgv@@163.com>
2026-08-03 18:57:38 +0800

    Site updated: 2026-08-03 18:57:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/30/创建博客/","2026/07/30/创建博客进阶/","2026/07/30/hello-world/"];function toRandomPost(){
+var posts=["2026/07/30/创建博客进阶/","2026/07/30/hello-world/","2026/07/30/创建博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
