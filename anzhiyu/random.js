8a66618e43a6761ecd95a059bc07aade3278a14a
w5yuu <g73ubgv@@163.com>
2026-08-04 18:01:14 +0800

    Site updated: 2026-08-04 18:01:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/30/hello-world/","2026/07/30/创建博客/","2026/07/30/创建博客进阶/","2026/08/04/问题解决1（图片无法加载）/"];function toRandomPost(){
+var posts=["2026/07/30/创建博客/","2026/07/30/创建博客进阶/","2026/08/04/问题解决1（图片无法加载）/","2026/07/30/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
