67602ca3c6c5466c1e05c8d6bf1b0e19a492046b
w5yuu <g73ubgv@@163.com>
2026-08-09 18:54:05 +0800

    Site updated: 2026-08-09 18:54:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/30/创建博客进阶/","2026/07/30/创建博客/","2026/08/04/问题解决1（图片无法加载）/","2026/07/30/hello-world/","2026/08/08/pwn35-40/"];function toRandomPost(){
+var posts=["2026/08/08/pwn35-40/","2026/07/30/创建博客/","2026/07/30/hello-world/","2026/08/09/pwn41-46/","2026/07/30/创建博客进阶/","2026/08/04/问题解决1（图片无法加载）/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
